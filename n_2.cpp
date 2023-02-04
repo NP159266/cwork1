59b4c503b96b9599994b977901c13c2574df9967
unknown <np3158901@gmail.com>
2023-02-04 21:20:04 +0300

    inicial commit

---
+++ n_2.cpp
@@ -0,0 +1,8 @@
+#include "task_1.hpp"
+int main() {
+    int ar[1000];
+    fill(ar, 100);
+    bubbleSort(ar, 100);
+    show(ar, 100);
+    cout << ar[cMax(ar, 100)] << ": " << cDig(ar, 100, cMax(ar, 100)) << " | " << ar[cMin(ar, 100)] << ": " << cDig(ar, 100, cMin(ar, 100)) << endl;
+}
