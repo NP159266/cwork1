c9338b64e09556b909253b69140f0e0e92dd06fc
unknown <np3158901@gmail.com>
2023-02-04 14:40:50 +0300

    inicial commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <iostrean>
+int main(){
+	for (int i = 0, i < 10){
+		std::cout<<i;
+		i=i+1;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-usig namespace std;
 int main(){
-	for (int i=0, i<10, i++){
-		cout<<i<<"\n";
+	for (int i = 0, i < 10, i++){
+		std::cout<<i<<"\n";
 	}
 }
 
