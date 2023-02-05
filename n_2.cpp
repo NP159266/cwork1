8fd24c8d61527efc72af651b200b24894e00b402
Test1Denis <72029911+Test1Denis@users.noreply.github.com>
2023-02-05 19:34:44 +0300

    Update n_2.cpp

--- n_2.cpp
+++ n_2.cpp
@@ -2,7 +2,7 @@
 int main() {
     int ar[1000];
     fill(ar, 100);
-    bubbleSort(ar, 100);
-    show(ar, 100);
+    bubbleSort(ar, 100);    <-- не принимаается
+    show(ar, 100);  <-- если это задание номер 1, то кажется эта функция написана немного не так
     cout << ar[cMax(ar, 100)] << ": " << cDig(ar, 100, cMax(ar, 100)) << " | " << ar[cMin(ar, 100)] << ": " << cDig(ar, 100, cMin(ar, 100)) << endl;
 }
